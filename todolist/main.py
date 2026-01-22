85dbcacf73b9a9189a22e561abc616e9f49f211e
ygrec4ka <sam06ser@mail.ru>
2026-01-22 20:14:08 +0400

    create logging base config

--- todolist/main.py
+++ todolist/main.py
@@ -1,8 +1,15 @@
 import uvicorn
+import logging
 from fastapi import FastAPI
 
 from api import router as api_router
 from core.config import settings
+
+logging.basicConfig(
+    level=settings.logging.log_level_value,
+    format=settings.logging.log_format,
+)
+
 
 app = FastAPI()
 
