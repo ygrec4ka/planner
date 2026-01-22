f2443a0632b1c26e480caca65cc9957c77a8bcb4
ygrec4ka <sam06ser@mail.ru>
2026-01-22 20:18:26 +0400

    add user data to wh_data

--- todolist/utils/webhooks/user.py
+++ todolist/utils/webhooks/user.py
@@ -1,6 +1,7 @@
 import aiohttp
 import logging
 from core.models import User
+from core.schemas.user import UserRead
 
 log = logging.getLogger(__name__)
 
@@ -8,7 +9,9 @@ WEBHOOK_URL = "https://httpbin.org/post"
 
 
 async def send_new_user_notification(user: User) -> None:
-    wh_data = {}
+    wh_data = {
+        "user": UserRead.model_validate(user).model_dump(),
+    }
     log.info("Notify user created with data: %s", wh_data)
     async with aiohttp.ClientSession() as session:
         async with session.post(WEBHOOK_URL, json=wh_data) as response:
