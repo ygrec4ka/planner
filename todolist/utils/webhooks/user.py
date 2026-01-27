49643bb722783df4a1dc43816cc750a918da3916
ygrec4ka <sam06ser@mail.ru>
2026-01-27 12:39:50 +0400

    init new User Registered Notification schema to wh_data

--- todolist/utils/webhooks/user.py
+++ todolist/utils/webhooks/user.py
@@ -1,7 +1,9 @@
+import time
+
 import aiohttp
 import logging
 from core.models import User
-from core.schemas.user import UserRead
+from core.schemas.user import UserRead, UserRegisteredNotification
 
 log = logging.getLogger(__name__)
 
@@ -9,9 +11,10 @@ WEBHOOK_URL = "https://httpbin.org/post"
 
 
 async def send_new_user_notification(user: User) -> None:
-    wh_data = {
-        "user": UserRead.model_validate(user).model_dump(),
-    }
+    wh_data = UserRegisteredNotification(
+        user=UserRead.model_validate(user),
+        ts=int(time.time()),
+    ).model_dump()
     log.info("Notify user created with data: %s", wh_data)
     async with aiohttp.ClientSession() as session:
         async with session.post(WEBHOOK_URL, json=wh_data) as response:
