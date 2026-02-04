faa8d35aeca20d5128128633fdaea60f90baf47e
ygrec4ka <sam06ser@mail.ru>
2026-02-04 23:24:00 +0400

    create comment models

--- todolist/core/models/comments.py
+++ todolist/core/models/comments.py
@@ -1,40 +1,43 @@
-# from datetime import datetime
-# from typing import TYPE_CHECKING, Optional
-#
-# from core.models.base import Base
-# from sqlalchemy import Text, DateTime, ForeignKey
-# from sqlalchemy.orm import Mapped, mapped_column, relationship
-# from sqlalchemy.sql import func
-#
-# if TYPE_CHECKING:
-#     from core.models import User
-#     from core.models import Task
-#     from core.models import Note
-#
-#
-# class Comment(Base):
-#     __tablename__ = "comments"
-#
-#     content: Mapped[str] = mapped_column(Text, nullable=False)
-#     # Таймстемпы
-#     created_at: Mapped[datetime] = mapped_column(
-#         DateTime(timezone=True), server_default=func.now()
-#     )
-#     updated_at: Mapped[datetime] = mapped_column(
-#         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
-#     )
-#     # Связи
-#     user_id: Mapped[int] = mapped_column(
-#         ForeignKey("users.id", ondelete="CASCADE"), nullable=False
-#     )
-#     task_id: Mapped[int | None] = mapped_column(
-#         ForeignKey("tasks.id", ondelete="CASCADE"), nullable=True
-#     )
-#     note_id: Mapped[int | None] = mapped_column(
-#         ForeignKey("notes.id", ondelete="CASCADE"), nullable=True
-#     )
-#
-#     # Отношения
-#     user: Mapped["User"] = relationship(back_populates="comments")
-#     task: Mapped[Optional["Task"]] = relationship(back_populates="comments")
-#     note: Mapped[Optional["Note"]] = relationship(back_populates="comments")
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Text, DateTime, func, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from core.models import Base
+from core.models.mixins.id_int_pk import IdIntPkMixin
+
+if TYPE_CHECKING:
+    from core.models import User
+
+
+class Comment(Base, IdIntPkMixin):
+    content: Mapped[str] = mapped_column(
+        Text,
+        nullable=True,
+    )
+    commentable_type: Mapped[str] = mapped_column(
+        String(50),
+        nullable=False,
+    )
+    commentable_id: Mapped[int] = mapped_column(
+        nullable=False,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+    user: Mapped["User"] = relationship(back_populates="comments")
