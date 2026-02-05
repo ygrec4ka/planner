df0963c5fabbb4ed121e33246c71c5556e8979f0
ygrec4ka <sam06ser@mail.ru>
2026-02-05 22:01:34 +0400

    create "create_note" router

--- todolist/api/api_v1/notes.py
+++ todolist/api/api_v1/notes.py
@@ -99,3 +99,37 @@
 #     )
 #
 #     return {"message": "Note deleted successfully"}
+from typing import List
+from fastapi import APIRouter, Depends
+
+from api.dependencies.note.note import note_getter
+from core.authentication.fastapi_users import current_user
+from core.config import settings
+
+from core.models import User, Note
+
+from core.schemas.comment import CommentResponse, CommentCreate
+from core.schemas.note import NoteCreate, NoteResponse, NoteUpdate
+
+from services.comments import CommentService
+from services.notes import NoteService
+
+
+router = APIRouter(
+    prefix=settings.api.v1.notes,
+    tags=["Notes"],
+)
+
+
+@router.get("/", response_model=NoteResponse)
+async def create_note(
+    note_data: NoteCreate,
+    service: NoteService = Depends(NoteService),
+    user: User = Depends(current_user),
+):
+    return await service.create_note(
+        note_data=note_data,
+        user=user,
+    )
+
+
