515160e05c17f8b8d198e4cd42a5f7afb5e3f1c0
ygrec4ka <sam06ser@mail.ru>
2026-02-05 22:42:24 +0400

    create "update_comment_for_note" and "delete_comment_for_note" routers

--- todolist/api/api_v1/notes.py
+++ todolist/api/api_v1/notes.py
@@ -1,13 +1,15 @@
 from typing import List
 from fastapi import APIRouter, Depends
 
+from api.dependencies.comment.comment import comment_getter
 from api.dependencies.note.note import note_getter
+from api.dependencies.services.service import get_note_service, get_comment_service
 from core.authentication.fastapi_users import current_user
 from core.config import settings
 
-from core.models import User, Note
+from core.models import User, Note, Comment
 
-from core.schemas.comment import CommentResponse, CommentCreate
+from core.schemas.comment import CommentResponse, CommentCreate, CommentUpdate
 from core.schemas.note import NoteCreate, NoteResponse, NoteUpdate
 
 from services.comments import CommentService
@@ -20,10 +22,10 @@ router = APIRouter(
 )
 
 
-@router.get("/", response_model=NoteResponse)
+@router.post("/", response_model=NoteResponse)
 async def create_note(
     note_data: NoteCreate,
-    service: NoteService = Depends(NoteService),
+    service: NoteService = Depends(get_note_service),
     user: User = Depends(current_user),
 ):
     return await service.create_note(
@@ -35,7 +37,7 @@ async def create_note(
 @router.get("/{note_id}", response_model=NoteResponse)
 async def get_note(
     note_id: int,
-    service: NoteService = Depends(NoteService),
+    service: NoteService = Depends(get_note_service),
     user: User = Depends(current_user),
 ):
     return await service.get_note_by_id(
@@ -46,7 +48,7 @@ async def get_note(
 
 @router.get("/", response_model=List[NoteResponse])
 async def get_all_notes(
-    service: NoteService = Depends(NoteService),
+    service: NoteService = Depends(get_note_service),
     user: User = Depends(current_user),
 ):
     return await service.get_notes(
@@ -58,7 +60,7 @@ async def get_all_notes(
 async def update_note(
     note_data: NoteUpdate,
     note: Note = Depends(note_getter),
-    service: NoteService = Depends(NoteService),
+    service: NoteService = Depends(get_note_service),
 ):
     return await service.update_note(
         note=note,
@@ -69,7 +71,7 @@ async def update_note(
 @router.delete("/{note_id}")
 async def delete_note(
     note: Note = Depends(note_getter),
-    service: NoteService = Depends(NoteService),
+    service: NoteService = Depends(get_note_service),
 ):
     return await service.delete_note(
         note=note,
@@ -80,10 +82,10 @@ async def delete_note(
 async def create_comment_for_note(
     note_id: int,
     comment_data: CommentCreate,
-    service: CommentService = Depends(CommentService),
+    service: CommentService = Depends(get_comment_service),
     user: User = Depends(current_user),
 ):
-    await service.create_note_comment(
+    return await service.create_note_comment(
         comment_data=comment_data,
         user=user,
         note_id=note_id,
@@ -93,10 +95,32 @@ async def create_comment_for_note(
 @router.get("/{note_id}/comments", response_model=List[CommentResponse])
 async def get_comments_for_note(
     note_id: int,
-    service: CommentService = Depends(CommentService),
+    service: CommentService = Depends(get_comment_service),
     user: User = Depends(current_user),
 ):
     return await service.get_note_comments(
         user_id=user.id,
         note_id=note_id,
+    )
+
+
+@router.patch("/comment/{comment_id}", response_model=CommentResponse)
+async def update_comment_for_note(
+    comment_data: CommentUpdate,
+    comment: Comment = Depends(comment_getter),
+    service: CommentService = Depends(get_comment_service),
+):
+    return await service.update_comment(
+        comment_data=comment_data,
+        comment=comment,
+    )
+
+
+@router.delete("/comment/{comment_id}")
+async def delete_comment_for_note(
+    comment: Comment = Depends(comment_getter),
+    service: CommentService = Depends(get_comment_service),
+):
+    return await service.delete_comment(
+        comment=comment,
     )
